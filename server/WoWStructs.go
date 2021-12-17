d9abec8c9b6b3f7951c607d5b1c8b79c6f80d66d
ali <a_icam@yahoo.com>
2021-12-17 23:58:52 +0330

    wow structs

--- server/WoWStructs.go
+++ server/WoWStructs.go
@@ -7,6 +7,12 @@ type Hero struct {
 	Level  int    `json:"level"`
 	Class  int    `json:"class"`
 }
-
+type HeroPosition struct {
+	PositionX float32 `json:"position_x"`
+	PositionY float32 `json:"position_y"`
+	PositionZ float32 `json:"position_z"`
+}
 type Home struct {
+	Alliance HeroPosition `json:"alliance"`
+	Horde    HeroPosition `json:"horde"`
 }
