72fcde7b039f95297e53e67974983ae4a32f4e5d
ali <a_icam@yahoo.com>
2022-01-10 11:08:11 +0330

    cash system

--- test/test.go
+++ test/test.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
+
+func checkErr(e error) {
+	if e != nil {
+		log.Println(e)
+	}
+}
+
+func MD5(text string) string {
+	algorithm := md5.New()
+	algorithm.Write([]byte(text))
+	return hex.EncodeToString(algorithm.Sum(nil))
+}
 
 func main() {
 	router := gin.Default()
 
-	// This handler will match /user/john but will not match /user/ or /user
+	// This handler will match all requests
 	router.GET("/*url", func(c *gin.Context) {
 		url := c.Param("url")
+		url = url[1:]
+		cashFile := MD5(url) + ".txt"
 		c.Writer.Header().Set("Content-Type", "application/json")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Control-Allow-Origin", "*")
@@ -20,8 +38,12 @@ func main() {
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max, username")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		log.Println(url[1:])
-		resp, err := http.Get(url[1:])
+		if _, err := os.Stat("test/cash/" + cashFile); !errors.Is(err, os.ErrNotExist) {
+			f, _ := os.ReadFile("test/cash/" + cashFile)
+			c.String(http.StatusOK, string(f))
+			return
+		}
+		resp, err := http.Get(url)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -32,6 +54,11 @@ func main() {
 		}
 		//Convert the body to type string
 		sb := string(body)
+		f, err := os.Create("test/cash/" + cashFile)
+		checkErr(err)
+		_, err = f.Write(body)
+		checkErr(err)
+
 		c.String(http.StatusOK, sb)
 
 	})
