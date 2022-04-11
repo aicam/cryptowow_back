046e109a6591b422f01787a44465d11524613009
ali <a_icam@yahoo.com>
2022-04-11 22:30:54 +0430

    feat: redis

--- testDic/main.go
+++ testDic/main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
-	"github.com/aicam/cryptowow_back/server/Bridge"
-	"os"
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
 )
 
+var ctx = context.Background()
+
 func main() {
-	arenaFile := os.Getenv("CUSTOMARENAFILELOC")
-	params := Bridge.NewGameParams{
-		ArenaFilePath: arenaFile,
-		TeamID1:       1,
-		TeamID2:       2,
-		LeaderName1:   "Matarsak",
-		LeaderName2:   "Koonde",
-		ArenaType:     "2",
-		//MapType:       "4",
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
 	}
-	Bridge.AppendNewGame(params)
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
 }
